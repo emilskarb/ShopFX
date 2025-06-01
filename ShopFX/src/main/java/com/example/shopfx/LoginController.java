9b00a3f6713653879f463771a8cfe2592e9f0820
OLDXEMI\Emil <xemil2004@gmail.com>
2025-06-01 16:11:01 +0200

    Dodano obiekt user i walidację danych użytkownika przy logowaniu

--- ShopFX/src/main/java/com/example/shopfx/LoginController.java
+++ ShopFX/src/main/java/com/example/shopfx/LoginController.java
@@ -1,14 +1,20 @@
 package com.example.shopfx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginController {
+
     @FXML
-    private Button login_button;
+    public Button login_button;
 
     @FXML
     private TextField login_id;
@@ -17,8 +23,72 @@ public class LoginController {
     private TextField password_id;
 
     @FXML
-    void handleLogin(ActionEvent event) {
+    private List<User> users;
 
+    @FXML
+    public void handleLogin() {         //Funkcja jest odpowiedzialna za walidację danych i przejśćie do strony sklepu po wpisaniu odpowiednich danych
+        String login = login_id.getText();
+        String password = password_id.getText();
+
+        boolean userFound = users.stream().anyMatch(user -> user.getLogin().equals(login) && user.getPassword().equals(password));      //Szukamy w liście użytkowników
+
+        if (userFound) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("shop-view.fxml"));
+                Scene mainScene = new Scene(fxmlLoader.load(), 720, 640);
+                Stage stage = (Stage) login_button.getScene().getWindow();
+                stage.setScene(mainScene);
+                stage.setTitle("Panel sklepu");
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (login.isEmpty() || password.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Błąd logowania");
+            alert.setHeaderText(null);
+            alert.setContentText("Pola login i hasło nie mogą być puste!");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Błąd logowania");
+            alert.setHeaderText(null);
+            alert.setContentText("Niepoprawne dana logowania");
+            alert.showAndWait();
+        }
     }
+
+    public class User {
+        private String login;
+        private String password;
+
+        public User(String login, String password) {
+            this.login = login;
+            this.password = password;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        users = new ArrayList<>();
+        users.add(new User("admin", "admin"));
+        users.add(new User("login1", "password1"));
+        users.add(new User("login2", "password2"));
+    }
+
 }
 
--- ShopFX/src/main/java/com/example/shopfx/ShopController.java
+++ ShopFX/src/main/java/com/example/shopfx/ShopController.java
@@ -10,38 +10,7 @@ import javafx.scene.control.Button;
 import java.io.IOException;
 
 public class ShopController {
-    @FXML
-    public Button login_button;
 
-    @FXML
-    private TextField login_id;
 
-    @FXML
-    private TextField password_id;
 
-    @FXML
-    public void handleLogin() {         //Funkcja jest odpowiedzialna za walidację danych i przejśćie do strony sklepu po wpisaniu odpowiednich danych
-        String login = login_id.getText();
-        String password = password_id.getText();
-
-        if (login.equals("admin") && password.equals("admin")) {
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("shop-view.fxml"));
-                Scene mainScene = new Scene(fxmlLoader.load(), 720, 640);
-                Stage stage = (Stage) login_button.getScene().getWindow();
-                stage.setScene(mainScene);
-                stage.setTitle("Panel sklepu");
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Błąd logowania");
-                alert.setHeaderText(null);
-                alert.setContentText("Niepoprawne dana logowania");
-                alert.showAndWait();
-        }
-    }
 }

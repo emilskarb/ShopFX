dfab4b2bfb603a4c99a96db9c950461a9a9dc720
OLDXEMI\Emil <xemil2004@gmail.com>
2025-06-01 14:37:05 +0200

    Dodano walidację danych użytkownika

--- ShopFX/src/main/java/com/example/shopfx/ShopController.java
+++ ShopFX/src/main/java/com/example/shopfx/ShopController.java
@@ -3,28 +3,45 @@ package com.example.shopfx;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import java.io.IOException;
-
 
 public class ShopController {
     @FXML
     public Button login_button;
 
     @FXML
-    public void handleLogin() {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
-            Scene mainScene = new Scene(fxmlLoader.load());
+    private TextField login_id;
 
-            Stage stage = (Stage) login_button.getScene().getWindow();
-            stage.setScene(mainScene);
-            stage.setTitle("Panel sklepu");
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
+    @FXML
+    private TextField password_id;
+
+    @FXML
+    public void handleLogin() {         //Funkcja jest odpowiedzialna za walidację danych i przejśćie do strony sklepu po wpisaniu odpowiednich danych
+        String login = login_id.getText();
+        String password = password_id.getText();
+
+        if (login.equals("admin") && password.equals("admin")) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("shop-view.fxml"));
+                Scene mainScene = new Scene(fxmlLoader.load(), 720, 640);
+                Stage stage = (Stage) login_button.getScene().getWindow();
+                stage.setScene(mainScene);
+                stage.setTitle("Panel sklepu");
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Błąd logowania");
+                alert.setHeaderText(null);
+                alert.setContentText("Niepoprawne dana logowania");
+                alert.showAndWait();
         }
     }
-
 }

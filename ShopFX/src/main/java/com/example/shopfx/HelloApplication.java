4b77873aea7e78608754cca637bb7449c462adfb
emilskarb <xemil2004@gmail.com>
2025-06-15 17:43:43 +0200

    Zmieniono kolory na mniej ciepłe

--- ShopFX/src/main/java/com/example/shopfx/HelloApplication.java
+++ ShopFX/src/main/java/com/example/shopfx/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),620, 540 ); //320, 240
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/shopfx/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load()); //620, 540
         stage.setTitle("Shop");
         stage.setScene(scene);
         stage.show();
